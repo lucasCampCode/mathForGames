3b1f321b059e4823db2806f9e3f281c534c8456c
lucas <lcampbell0815@gmail.com>
2020-11-02 18:51:54 -0600

    added transform matrices

--- MathForGames/Actor.cs
+++ MathForGames/Actor.cs
@@ -11,8 +11,12 @@ namespace MathForGames
         protected char _icon = ' ';
         protected Vector2 _velocity;
         protected Matrix3 _transform;
+        private Matrix3 _translation = new Matrix3();
+        private Matrix3 _rotation = new Matrix3();
+        private Matrix3 _scale = new Matrix3();
         protected ConsoleColor _color;
         protected Color _rayColor;
+        protected Sprite _sprite;
         public bool Started { get; private set; }
 
         public Vector2 Forward
@@ -21,15 +25,8 @@ namespace MathForGames
             {
                 return new Vector2(_transform.m11, _transform.m21);
             }
-            set
-            {
-                _transform.m11 = value.X;
-                _transform.m21 = value.Y;
-            }
 
         }
-
-
         public Vector2 Position
         {
             get
@@ -38,11 +35,10 @@ namespace MathForGames
             }
             set
             {
-                _transform.m13 = value.X;
-                _transform.m23 = value.Y;
+                _translation.m13 = value.X;
+                _translation.m23 = value.Y;
             }
         }
-
         public Vector2 Velocity
         {
             get
@@ -68,7 +64,7 @@ namespace MathForGames
             Position = new Vector2(x, y);
             _velocity = new Vector2();
             _color = color;
-            Forward = new Vector2(1, 0);
+            _sprite = new Sprite("Images/enemy.png");
         }
 
 
@@ -84,18 +80,33 @@ namespace MathForGames
             _rayColor = rayColor;
         }
 
-        /// <summary>
-        /// Updates the actors forward vector to be
-        /// the last direction it moved in
-        /// </summary>
-        private void UpdateFacing()
+        public void SetTranslation(Vector2 pos)
         {
-            if (_velocity.Magnitude <= 0)
-                return;
-
-            Forward = Velocity.Normalized;
+            _translation.m13 = pos.X;
+            _translation.m23 = pos.Y;
         }
 
+        public void SetRotation(float radians)
+        {
+            _rotation.m11 = (float)Math.Cos(radians);
+            _rotation.m12 = (float)Math.Sin(radians);
+            _rotation.m21 = -(float)Math.Sin(radians);
+            _rotation.m22 = (float)Math.Cos(radians);
+        }
+        public void SetScale(float x,float y)
+        {
+            _scale.m11 = x;
+            _scale.m22 = y;
+        }
+
+        public void UpdateTransform()
+        {
+            //SetTranslation(new Vector2(1,1));
+            SetRotation(0);
+            SetScale(1,1);
+
+            _transform = _translation * _rotation * _scale;
+        }
         public virtual void Start()
         {
             Started = true;
@@ -104,9 +115,7 @@ namespace MathForGames
 
         public virtual void Update(float deltaTime)
         {
-            //Before the actor is moved, update the direction it's facing
-            UpdateFacing();
-
+            UpdateTransform();
             //Increase position by the current velocity
             Position += _velocity * deltaTime;
         }
@@ -115,6 +124,7 @@ namespace MathForGames
         {
             //Draws the actor and a line indicating it facing to the raylib window.
             //Scaled to match console movement
+            _sprite.Draw(_transform);
             Raylib.DrawText(_icon.ToString(), (int)(Position.X * 32), (int)(Position.Y * 32), 32, _rayColor);
             Raylib.DrawLine(
                 (int)(Position.X * 32),
--- MathForGames/Player.cs
+++ MathForGames/Player.cs
@@ -29,7 +29,7 @@ namespace MathForGames
         public Player(float x, float y, char icon = ' ', ConsoleColor color = ConsoleColor.White)
             : base(x, y, icon, color)
         {
-
+            _sprite = new Sprite("Images/player.png");
         }
 
         /// <param name="x">Position on the x axis</param>
@@ -40,7 +40,7 @@ namespace MathForGames
         public Player(float x, float y, Color rayColor, char icon = ' ', ConsoleColor color = ConsoleColor.White)
             : base(x, y, rayColor, icon, color)
         {
-
+            _sprite = new Sprite("Images/player.png");
         }
 
         public override void Update(float deltaTime)
